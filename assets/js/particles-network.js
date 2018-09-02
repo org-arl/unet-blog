db6a40dacda0e97ae3e302c0a04efcfbd99a1ee3
Chinmay Pendharkar <notthetup@gmail.com>
2018-09-02 15:48:59 +0800

    added particles config

---
+++ assets/js/particles-network.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  particlesJS("main", {
+    particles: {
+      number: { value: 80, density: { enable: true, value_area: 800 } },
+      color: { value: "#ffffff" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#000000" },
+        polygon: { nb_sides: 5 },
+        image: { src: "img/github.svg", width: 100, height: 100 }
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 3,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "grab" },
+        onclick: { enable: true, mode: "bubble" },
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 158.35505639876231,
+          line_linked: { opacity: 0.20565637287968297 }
+        },
+        bubble: {
+          distance: 121.81158184520176,
+          size: 12.181158184520177,
+          duration: 2,
+          opacity: 0.6334202255950492,
+          speed: 3
+        },
+        repulse: { distance: 200, duration: 0.4 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 }
+      }
+    },
+    retina_detect: true
+  });
+});
