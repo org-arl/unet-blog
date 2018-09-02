3f4807616bec334f302ef081fee112ca28a0a5d0
Chinmay Pendharkar <notthetup@gmail.com>
2018-09-02 16:36:41 +0800

    added particles and fixed mobile responsiveness

--- assets/js/particles-network.js
+++ assets/js/particles-network.js
@@ -1,40 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   particlesJS("main", {
     particles: {
-      number: { value: 80, density: { enable: true, value_area: 800 } },
+      number: { value: 40, density: { enable: true, value_area: 500 } },
       color: { value: "#ffffff" },
       shape: {
-        type: "circle",
-        stroke: { width: 0, color: "#000000" },
-        polygon: { nb_sides: 5 },
-        image: { src: "img/github.svg", width: 100, height: 100 }
+        type: "circle"
       },
       opacity: {
         value: 0.5,
-        random: false,
-        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+        random: true,
+        anim: { enable: true, speed: 0.2, opacity_min: 0.1, sync: false }
       },
       size: {
         value: 3,
-        random: true,
-        anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+        random: true
       },
       line_linked: {
         enable: true,
-        distance: 150,
+        distance: 200,
         color: "#ffffff",
-        opacity: 0.4,
-        width: 1
+        opacity: 0.6,
+        width: 1.5
       },
       move: {
         enable: true,
-        speed: 3,
+        speed: 0.5,
         direction: "none",
-        random: false,
+        random: true,
         straight: false,
-        out_mode: "out",
-        bounce: false,
-        attract: { enable: false, rotateX: 600, rotateY: 1200 }
+        out_mode: "out"
       }
     },
     interactivity: {
@@ -46,19 +40,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
       },
       modes: {
         grab: {
-          distance: 158.35505639876231,
-          line_linked: { opacity: 0.20565637287968297 }
+          distance: 160,
+          line_linked: { opacity: 0.5 }
         },
         bubble: {
-          distance: 121.81158184520176,
-          size: 12.181158184520177,
+          distance: 120,
+          size: 6,
           duration: 2,
-          opacity: 0.6334202255950492,
+          opacity: 0.7,
           speed: 3
-        },
-        repulse: { distance: 200, duration: 0.4 },
-        push: { particles_nb: 4 },
-        remove: { particles_nb: 2 }
+        }
       }
     },
     retina_detect: true
