d1a2a9683bd42a372b3b992de518e3cf64304ece
Mandar Chitre <mandar@nus.edu.sg>
2020-11-02 00:56:32 +0800

    fixed julia logo colors

--- support/mk-custom-julia-phy-banner.jl
+++ support/mk-custom-julia-phy-banner.jl
@@ -18,8 +18,8 @@ let p = plot(f, 0, 10; size=(1300,500), color=RGBA(1,0,0,0.5), legend=false)
   for a ∈ 1.0:-0.001:0.7
     plot!(p, x -> a*f1(x), 0, 10; color=RGBA(0,0,1,0.03), legend=false)
   end
-  scatter!(p, [1.0], [-0.02]; markersize=10, markercolor=:green, markeralpha=0.5, markerstrokecolor=:black, legend=false)
-  scatter!(p, [1.1], [-0.12]; markersize=10, markercolor=:blue, markeralpha=0.5, markerstrokecolor=:black, legend=false)
-  scatter!(p, [0.9], [-0.12]; markersize=10, markercolor=:red, markeralpha=0.5, markerstrokecolor=:black, legend=false)
+  scatter!(p, [2.7], [0.5]; markersize=10, markercolor=RGB(0.220,0.596,0.149), markeralpha=0.5, markerstrokecolor=:black, legend=false)
+  scatter!(p, [2.8], [0.4]; markersize=10, markercolor=RGB(0.584,0.345,0.698), markeralpha=0.5, markerstrokecolor=:black, legend=false)
+  scatter!(p, [2.6], [0.4]; markersize=10, markercolor=RGB(0.796,0.235,0.200), markeralpha=0.5, markerstrokecolor=:black, legend=false)
   p
 end
